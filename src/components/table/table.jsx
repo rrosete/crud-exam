1a64c95ce286732908a151797bff8350fbe7d9ae
Ryan Rosete <r.ryancedrick1993@gmail.com>
2025-08-12 15:31:42 +0800

    Added table component

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -1,5 +1,33 @@
 import React from "react";
 
-export const Table = () => {
-  return <div>table</div>;
+export const Table = ({ data, columns }) => {
+  return (
+    <div className="overflow-x-auto">
+      <table className="bg-white border border-gray-200 w-full">
+        <thead>
+          <tr className="bg-gray-200 uppercase text-sm leading-normal">
+            {columns.map((column, index) => (
+              <th key={index} className="py-3 px-6 text-left">
+                {column}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody className="text-sm font-light">
+          {data.map((row, rowIndex) => (
+            <tr
+              key={rowIndex}
+              className="border-b border-gray-200 hover:bg-gray-100"
+            >
+              {columns.map((column, colIndex) => (
+                <td key={colIndex} className="py-3 px-6 capitalize">
+                  {row[column]}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
--- src/modules/admin/post/index.jsx
+++ src/modules/admin/post/index.jsx
@@ -1,11 +1,32 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { Button } from "components/button/button";
+import { Table } from "components/table/table";
 
 export const PostPage = () => {
+  const router = useRouter();
+  const sampleData = [
+    {
+      name: "Ryan Smith",
+      comment: "I like comment",
+      time: "11:30 PM",
+    },
+  ];
+  const sampleHeader = ["name", "comment", "time"];
+
+  const handleOnclick = () => {
+    router.push("/post/create");
+  };
+
   return (
-    <div>
-      <Button variant="primary">Add Post</Button>
+    <div className="">
+      <Button variant="primary" onClick={handleOnclick}>
+        Add Post
+      </Button>
+      <div className="mt-5">
+        <Table data={sampleData} columns={sampleHeader} />
+      </div>
     </div>
   );
 };
